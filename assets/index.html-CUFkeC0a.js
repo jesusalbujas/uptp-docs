ee82915366b5e98c6497ef182b249ff29704e95a
jesusalbujas <134967453+jesusalbujas@users.noreply.github.com>
2025-11-24 03:28:18 +0000

    Deploying to gh-pages from @ jesusalbujas/uptp-docs@1772af754d34e78e59a6817e0180f92a709f6072 🚀

---
+++ assets/index.html-CUFkeC0a.js
@@ -0,0 +1,31 @@
+import{_ as e}from"./plugin-vue_export-helper-DlAUqK2U.js";import{c as t,a as n,o}from"./app-CJfJTby7.js";const r={};function i(m,s){return o(),t("div",null,[n(` 
+## 🚀Usage
+
+::: code-tabs#language
+
+@tab TS
+
+\`\`\`ts title=".vuepress/config.ts"
+import { defineUserConfig } from "vuepress";
+import { hopeTheme } from "vuepress-theme-hope";
+
+export default defineUserConfig({
+  theme: hopeTheme({
+    // your theme config here
+  }),
+});
+\`\`\`
+
+@tab JS
+
+\`\`\`js title=".vuepress/config.js"
+import { hopeTheme } from "vuepress-theme-hope";
+
+export default {
+  theme: hopeTheme({
+    // your theme config here
+  }),
+};
+\`\`\`
+
+::: `)])}const c=e(r,[["render",i]]),h=JSON.parse('{"path":"/","title":"Home","lang":"en-US","frontmatter":{"home":true,"icon":"home","title":"Home","heroImage":"/assets/img/logo-menu.png","heroText":"IT Pst Documentation","tagline":"Universidad Politécnica Territorial del estado Portuguesa \\"Juan de Jesús Montilla\\"","actions":[{"text":"Get Started","link":"/get-started/","icon":"rocket","type":"primary"},{"text":"Guide","icon":"book-open","link":"/guide/"}],"copyright":false,"footer":"MIT Licensed | Copyright © 2024-present University Project","head":[["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"WebPage\\",\\"name\\":\\"Home\\"}"],["meta",{"property":"og:url","content":"https://github.com/adempiere/vue-ui-theme.github.io/docs/"}],["meta",{"property":"og:site_name","content":"UPTP"}],["meta",{"property":"og:title","content":"Home"}],["meta",{"property":"og:type","content":"website"}],["meta",{"property":"og:locale","content":"en-US"}],["meta",{"property":"og:locale:alternate","content":"es-ES"}],["meta",{"property":"og:updated_time","content":"2025-11-24T03:27:24.000Z"}],["meta",{"property":"article:modified_time","content":"2025-11-24T03:27:24.000Z"}],["link",{"rel":"alternate","hreflang":"es-es","href":"https://github.com/adempiere/vue-ui-theme.github.io/docs/es/"}]]},"git":{"createdTime":1763954844000,"updatedTime":1763954844000,"contributors":[{"name":"Jesús Albujas","username":"","email":"jesusramirez35000@gmail.com","commits":1}]},"readingTime":{"minutes":1.41,"words":423},"filePathRelative":"README.md","excerpt":"<!-- \\n## 🚀Usage\\n\\n::: code-tabs#language\\n\\n@tab TS\\n\\n```ts title=\\".vuepress/config.ts\\"\\nimport { defineUserConfig } from \\"vuepress\\";\\nimport { hopeTheme } from \\"vuepress-theme-hope\\";\\n\\nexport default defineUserConfig({\\n  theme: hopeTheme({\\n    // your theme config here\\n  }),\\n});\\n```\\n\\n@tab JS\\n\\n```js title=\\".vuepress/config.js\\"\\nimport { hopeTheme } from \\"vuepress-theme-hope\\";\\n\\nexport default {\\n  theme: hopeTheme({\\n    // your theme config here\\n  }),\\n};\\n```\\n\\n::: -->"}');export{c as comp,h as data};
